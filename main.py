8e08a1830a9c403623cd5d49b90a5f18bab70993
Komputer <firsttest888q@gmail.com>
2022-02-20 19:37:52 +0300

    x3_1

---
+++ main.py
@@ -0,0 +1,16 @@
+sp = []
+def input_valid():
+    pass
+
+def x2(x):
+    pass
+
+def x3_1(x):
+    x = int(x * 3 + 1)
+    sp.append(x)
+    collatz(x)
+
+def collatz(x):
+    pass
+
+input_valid()
