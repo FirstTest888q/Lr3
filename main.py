1d45623b22e37aea22b494de65d462865372574c
Komputer <firsttest888q@gmail.com>
2022-02-20 19:28:42 +0300

    input_valid

---
+++ main.py
@@ -0,0 +1,20 @@
+sp = []
+def input_valid():
+    while True:
+        x = int(input("Введите переменную x = "))
+        if x > 0:
+            collatz(x)
+            break
+        print("Неверное число, попробуйте другое")
+        return x
+
+def x2(x):
+    pass
+
+def x3_1(x):
+    pass
+
+def collatz(x):
+    pass
+
+input_valid()
